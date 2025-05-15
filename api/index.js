83cbfc1824f6716ac521cbdaa2f679eed0d49d63
josuegarrido <wocking589@gmail.com>
2025-05-15 03:00:26 -0500

    CORS

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ app.use(cors({
   origin: ['*'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  credentials: false
 }));
 
 // Middleware para parsear JSON
