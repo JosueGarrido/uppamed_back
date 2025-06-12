2215428b93e6109b52017c8c69754ce6e4653519
josuegarrido <wocking589@gmail.com>
2025-06-12 01:26:51 -0500

    uppamed

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 });
 
 // Configuración de CORS completamente abierta
-app.use(cors());
+//app.use(cors());
 
 // Middleware para parsear JSON
 app.use(express.json());
