c80514553a2787b56c7a34f63c089d62f38916b7
josuegarrido <wocking589@gmail.com>
2025-06-12 01:11:19 -0500

    uppamed

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ const cors = require('cors');
 dotenv.config();
 const app = express();
 
+// Middleware para forzar headers de CORS en todas las rutas
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 // CORS: Permitir solo el origen local y credenciales para desarrollo
 app.use(cors({
   origin: 'http://localhost:3000',
