4ba161901316a7f5680d7b182154c8ea84e69af7
josuegarrido <wocking589@gmail.com>
2025-06-12 01:20:31 -0500

    uppamed

--- api/index.js
+++ api/index.js
@@ -15,6 +15,19 @@ const medicalExamRoutes = require('../routes/medicalExamRoutes');
 dotenv.config();
 const app = express();
 
+// Middleware para forzar headers de CORS en todas las rutas (Vercel serverless)
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  next();
+});
+
 // Configuración de CORS completamente abierta
 app.use(cors());
 
