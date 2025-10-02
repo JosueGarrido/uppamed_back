493b84f09bb38f1011c68cb3f7e144aacae0ea03
Josue Garrido <38926331+JosueGarrido@users.noreply.github.com>
2025-10-02 15:29:35 -0500

    fix: Update migration to reference correct table name 'Tenants' instead of 'tenants'

--- migrations/20250115000005-create-medical-prescriptions.js
+++ migrations/20250115000005-create-medical-prescriptions.js
@@ -33,7 +33,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'tenants',
+          model: 'Tenants',
           key: 'id'
         },
         onUpdate: 'CASCADE',
